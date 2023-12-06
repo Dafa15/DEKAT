fe7b8f8e13d44334dccd201f80c8e350ca41b4f3
Dafa15 <dafaaldian25@gmail.com>
2023-12-06 14:24:38 +0700

    Add login,register and home activity

---
+++ app/src/main/java/com/capstone/dekat/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.capstone.dekat.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.dekat.R
+import com.capstone.dekat.ui.register.RegisterActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this@MainActivity, RegisterActivity::class.java))
+
+    }
+}
