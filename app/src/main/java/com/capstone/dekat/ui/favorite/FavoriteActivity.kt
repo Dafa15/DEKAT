25e0eda5da29e17cbf11f655d2c864317a2382e4
Dhika <dhikasyifas@gmail.com>
2023-12-13 16:29:28 +0700

    menambahkan result, detail, favorite

---
+++ app/src/main/java/com/capstone/dekat/ui/favorite/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.dekat.ui.favorite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.dekat.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
